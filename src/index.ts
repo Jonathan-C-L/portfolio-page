ceee1ea2beffa84b345d2c5a26b0b614e8d8b40c
Jonathan-C-L <jle16@nait.ca>
2026-05-01 20:15:57 -0600

    Initial start of typescript portfolio page project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+//-----------------------------------------------------
+// Author:  Jonathan Le
+// Purpose: Entry point for all other components
+//-----------------------------------------------------
+import app from "./server";
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
