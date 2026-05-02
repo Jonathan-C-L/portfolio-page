f59d7390507915656ece157fe01e8bf4e6608cf6
Jonathan-C-L <jle16@nait.ca>
2026-05-01 20:16:58 -0600

    Moved previous javascript code out of the typescript base - javascript code will be reintroduced on an as-needed basis

--- components/footer.js
+++
@@ -1,7 +0,0 @@
-console.log("footer.js connected");
-
-function RenderFooter(){
-    const copyright = `<p>&copy; Copyright 2025 Jonathan Le</p>`;
-    let credits = `<div class='credits'>CRT Effects: <a target='_blank' href='https://codepen.io/thisanimus/pen/OJpaqWz'>https://codepen.io/thisanimus/pen/OJpaqWz</a> (@thisanimus)</div>`;
-    AppendAll(footer, [copyright, credits]);
-}
--- components/nav.js
+++
@@ -1,9 +0,0 @@
-function showSection(id){
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-  // close mobile nav
-  document.getElementById("nav-links").classList.remove("open");
-}
-
-document.getElementById("hamburger").addEventListener("click", () => {
-  document.getElementById("nav-links").classList.toggle("open");
-});
