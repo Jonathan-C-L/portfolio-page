1662747df29d295e66694d370db6589a273e610d
Jonathan-C-L <jle16@nait.ca>
2026-05-06 14:37:25 -0600

    Updated naming of config object's allowed cross-origin resource sharing property

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,10 +16,10 @@ const CURRENT_VER: string = "v1";
 
 // Create config object from info parsed in .env file
 const config = {
-  env: process.env.NODE_ENV ?? DEFAULT_ENV,   // If null, coalesce to "development"
+  env: process.env.NODE_ENV ?? DEFAULT_ENV,             // If null, coalesce to "development"
   port: Number(process.env.PORT) || DEFAULT_PORT,       // Cast PORT as number, default to 3000 if none
-  apiVersion: process.env.API_VERSION ?? CURRENT_VER,  // If null, coalesce to "v1"
-  cor: {                                        // Defining Cross-Orgin Resources, default localhost:3000
+  apiVersion: process.env.API_VERSION ?? CURRENT_VER,   // If null, coalesce to "v1"
+  cors: {                                               // Defining Cross-Orgin Resources, default localhost:3000
     allowedOrigins: process.env.ALLOWED_ORIGINS?.split(',') ?? [`http://localhost:${DEFAULT_PORT}`]
   }
 };
