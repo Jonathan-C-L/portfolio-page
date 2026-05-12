63d684e92cfa20f5ded2cb922f967237edab50b3
Jonathan-C-L <jle16@nait.ca>
2026-05-12 15:01:38 -0600

    Implemented method for api version to between shared between client and server, so only 1 place needs to be changed

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { API_BASE_PATH } from '../../../shared/constants';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000/api/v1"
+    baseURL: `${import.meta.env.VITE_API_URL ?? "http://localhost:3000"}${API_BASE_PATH}`
 });
 
 // Exporting function
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,6 +5,7 @@
 // at a glance what environment variables your app depends on.
 //-----------------------------------------------------
 import dotenv from "dotenv";
+import { API_VERSION } from '../../../shared/constants';
 
 // Load environment variables from .env/.env.local
 dotenv.config(); 
@@ -12,13 +13,12 @@ dotenv.config();
 // Defaults for my development environment
 const DEFAULT_PORT: number = 3000;
 const DEFAULT_ENV: string = "development";
-const CURRENT_VER: string = "v1";
 
 // Create config object from info parsed in .env file
 const config = {
   env: process.env.NODE_ENV ?? DEFAULT_ENV,             // If null, coalesce to "development"
   port: Number(process.env.PORT) || DEFAULT_PORT,       // Cast PORT as number, default to 3000 if none
-  apiVersion: process.env.API_VERSION ?? CURRENT_VER,   // If null, coalesce to "v1"
+  apiVersion: API_VERSION,   
   cors: {                                               // Defining Cross-Orgin Resources, default localhost:3000
     allowedOrigins: process.env.ALLOWED_ORIGINS?.split(',') ?? [`http://localhost:${DEFAULT_PORT}`]
   }
