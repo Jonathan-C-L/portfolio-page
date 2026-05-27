871d0edfebb813f43a7275b3b59fdc110567e48b
Jonathan-C-L <jle16@nait.ca>
2026-05-27 15:35:31 -0600

    removed hardcoded text as data is pulled from github now

--- data/projects.js
+++ data/projects.js
@@ -1,39 +1,8 @@
-import { AjaxError, CallAjax } from "../library/ajax.js";
-import { ConvertToTitle } from "../library/lib.js";
-import config from "../config.js";
-
-// export async function ProjectsData(){
-//   return await GetProjects()
-
-//   return [
-//     {
-//       title: "To-Do List",
-//       id: "to-do",
-//       desc: "A To-Do list that uses the localStorage API to store items to be completed. Items can be filtered by time-frames (today, week, month, all).",
-//       learned: "Node.js, scripting, localStorage API, modular design, webpack",
-//       repo: "https://github.com/Jonathan-C-L/to-do-list"
-//     },
-//     {
-//       title: "Othello",
-//       id: "othello",
-//       desc: "Using PHP to perform server-side game logic processing for the classic board game Othello.",
-//       learned: "PHP, data cleaning, server-side validation, client-server communication",
-//       repo: "https://github.com/Jonathan-C-L/othello"
-//     },
-//     {
-//       title: "Demonstrations",
-//       id: "demos",
-//       desc: "Previous programming projects from school that display my programming style and document my progression as a developer.",
-//       learned: "OOP, REST APIs, Full-Stack web dev, .NET Framework, Embedded Systems, memory management, data structures, sorting algorithms",
-//       repo: "https://github.com/Jonathan-C-L/demonstrations"
-//     }
-//   ];
-// }
+import { ConvertToTitle, TitleCase } from "../library/lib.js";
 
 export async function ProjectsData(){
   const returnData = await fetch('https://api.github.com/users/Jonathan-C-L/repos', {
     headers: {
-      'Authorization': `Bearer ${config.GITHUB_TOKEN}`,
       'Accept': 'application/vnd.github+json',
       'X-GitHub-Api-Version': '2022-11-28'
     }
@@ -58,7 +27,7 @@ function ProcessData(rawData){
       title: ConvertToTitle(project["name"]),
       id: project["name"],
       desc: project["description"],
-      learned: project["topics"].map(topic => topic.toUpperCase()).join(", "),
+      learned: TitleCase(project["topics"]).join(", "),
       repo: project["html_url"]
     }
     
